a32af13833d8e5b78cf554574abbc50f2113c738
Abdul Rahman <programmerabubassam@gmail.com>
2025-09-21 18:53:48 +0300

    Update home data model

--- lib/features/home/data/models/home_data.g.dart
+++ lib/features/home/data/models/home_data.g.dart
@@ -14,10 +14,20 @@ HomeData _$HomeDataFromJson(Map<String, dynamic> json) => HomeData(
       books: (json['books'] as List<dynamic>)
           .map((e) => HomeBook.fromJson(e as Map<String, dynamic>))
           .toList(),
+      firstCategoryPageSize:
+          (json['meta:firstCategoryPageSize'] as num).toInt(),
+      totalFirstCategoryBooks:
+          (json['meta:totalFirstCategoryBooks'] as num).toInt(),
+      totalNewBooks: (json['meta:totalNewBooks'] as num).toInt(),
+      newBooksPageSize: (json['meta:newBooksPageSize'] as num).toInt(),
     );
 
 Map<String, dynamic> _$HomeDataToJson(HomeData instance) => <String, dynamic>{
       'notificationCount': instance.notificationCount,
       'categories': instance.categories,
       'books': instance.books,
+      'meta:firstCategoryPageSize': instance.firstCategoryPageSize,
+      'meta:totalFirstCategoryBooks': instance.totalFirstCategoryBooks,
+      'meta:totalNewBooks': instance.totalNewBooks,
+      'meta:newBooksPageSize': instance.newBooksPageSize,
     };
