16a037f6c65d45c71119a170aabd59e2d737078a
Abdul Rahman <programmerabubassam@gmail.com>
2025-09-21 18:53:17 +0300

    Add home failure

--- lib/core/helpers/failure.dart
+++ lib/core/helpers/failure.dart
@@ -3,3 +3,11 @@ abstract class MyFailure {
 
   const MyFailure({required this.message});
 }
+
+class HomeFailure extends MyFailure {
+  HomeFailure({required message})
+      : super(
+          message:
+              'Load data failed check your internet connection, and try again later',
+        );
+}
