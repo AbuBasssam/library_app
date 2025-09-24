7a568518a9d7846df36cf810564c9e6039785d3c
Abdul Rahman <programmerabubassam@gmail.com>
2025-09-24 06:18:31 +0300

    Update HomeData model

--- lib/features/home/data/models/home_data.dart
+++ lib/features/home/data/models/home_data.dart
@@ -10,23 +10,10 @@ class HomeData {
   final List<BookCategoryModel> categories;
   final List<HomeBook> books;
 
-  @JsonKey(name: 'meta:firstCategoryPageSize')
-  final int firstCategoryPageSize;
-  @JsonKey(name: 'meta:totalFirstCategoryBooks')
-  final int totalFirstCategoryBooks;
-  @JsonKey(name: 'meta:totalNewBooks')
-  final int totalNewBooks;
-  @JsonKey(name: 'meta:newBooksPageSize')
-  final int newBooksPageSize;
-
   HomeData({
     required this.notificationCount,
     required this.categories,
     required this.books,
-    required this.firstCategoryPageSize,
-    required this.totalFirstCategoryBooks,
-    required this.totalNewBooks,
-    required this.newBooksPageSize,
   });
   factory HomeData.fromJson(Map<String, dynamic> json) =>
       _$HomeDataFromJson(json);
