8c791ded8fe36fecd84f7ba622af93b046f1aace
Abdul Rahman <programmerabubassam@gmail.com>
2025-09-24 08:44:14 +0300

    Update CategoryBooksSection

--- lib/features/home/presentation/widgets/category_books_section.dart
+++ lib/features/home/presentation/widgets/category_books_section.dart
@@ -1,7 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import '../../../../core/helpers/spacing.dart';
+import 'package:library_app/core/helpers/spacing.dart';
 import '/features/book/domain/entities/book_entity.dart';
 import '/features/home/presentation/widgets/home_book_list.dart';
 import '/generated/locale_keys.g.dart';
@@ -13,39 +13,6 @@ class CategoryBooksSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<BookEntity> books = <BookEntity>[
-      BookEntity(
-        imageUrl: 'https://picsum.photos/160/202',
-        isNew: true,
-        title: 'Atomic Habits',
-        author: 'James Clear',
-      ),
-      BookEntity(
-        imageUrl: 'https://picsum.photos/160/202',
-        isNew: false,
-        title: 'The Subtle Art of Not Giving a F*ck',
-        author: 'Mark Manson',
-      ),
-      BookEntity(
-        imageUrl: 'https://picsum.photos/160/202',
-        isNew: true,
-        title: 'Deep Work',
-        author: 'Cal Newport',
-      ),
-      BookEntity(
-        imageUrl: 'https://picsum.photos/160/202',
-        isNew: false,
-        title: 'Thinking, Fast and Slow',
-        author: 'Daniel Kahneman',
-      ),
-      BookEntity(
-        imageUrl: 'https://picsum.photos/160/202',
-        isNew: true,
-        title: 'Grit',
-        author: 'Angela Duckworth',
-      ),
-    ];
-
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16.w),
       child: Column(
