66e2d7456a4d81191ceaf03d4643b8a577a86523
Abdul Rahman <programmerabubassam@gmail.com>
2025-09-24 06:19:34 +0300

    add BaseBookData model

---
+++ lib/features/home/data/models/base_book_data.dart
@@ -0,0 +1,22 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'base_book_data.g.dart';
+
+@JsonSerializable()
+class BaseBookData {
+  final int bookId;
+  final String title;
+  final String author;
+  final String coverImageUrl;
+  @JsonKey(name: 'isNewBook')
+  final bool isNew;
+  BaseBookData({
+    required this.bookId,
+    required this.title,
+    required this.author,
+    required this.coverImageUrl,
+    required this.isNew,
+  });
+  factory BaseBookData.fromJson(Map<String, dynamic> json) =>
+      _$BaseBookDataFromJson(json);
+}
