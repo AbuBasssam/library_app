e855b57ffc9ac8b30268c438ee5d032f198ba729
Abdul Rahman <programmerabubassam@gmail.com>
2025-09-24 06:51:29 +0300

    Update HomeDataEntity

--- lib/features/home/domain/entities/home_data_entity.dart
+++ lib/features/home/domain/entities/home_data_entity.dart
@@ -1,14 +1,19 @@
-import 'package:library_app/core/helpers/pagination_response.dart';
+import 'package:library_app/core/helpers/pagination/pagination_response.dart';
 import 'package:library_app/features/book/domain/entities/book_entity.dart';
+import 'package:library_app/features/home/domain/entities/book_category.dart';
 
 class HomeDataEntity {
-  final PaginationResult<BookEntity> newestBooksSection;
+  final int notificationCount;
+  final List<BookCategory> categories;
   final PaginationResult<BookEntity> categoryBooksSection;
   final List<BookEntity> mostPopularBooksSection;
+  final PaginationResult<BookEntity> newestBooksSection;
 
   HomeDataEntity({
-    required this.newestBooksSection,
+    required this.notificationCount,
+    required this.categories,
     required this.categoryBooksSection,
     required this.mostPopularBooksSection,
+    required this.newestBooksSection,
   });
 }
