8c4cdb0faf53db89f44532f3679ae3e765c311df
Abdul Rahman <programmerabubassam@gmail.com>
2025-10-08 15:55:59 +0300

    add enBookState extension

--- lib/core/helpers/extension.dart
+++ lib/core/helpers/extension.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:library_app/features/book/domain/entities/en_book_state.dart';
+
 import '../../features/book/domain/entities/book_entity.dart';
 import '../../features/book/domain/entities/cover_info.dart';
 import '../../features/book/presentation/view%20models/detail_view_info.dart';
@@ -105,4 +108,91 @@ extension BookEntityExtensions on BookEntity {
 
 extension StringExtension on String? {
   bool isNullOrEmpty() => this == null || this == "";
+}
+
+extension BookStateUI on enBookState {
+  String get displayText {
+    switch (this) {
+      case enBookState.borrowable:
+        return 'Borrowable';
+      case enBookState.borrowed:
+        return 'Borrowed';
+      case enBookState.reservable:
+        return 'قابل للحجز';
+      case enBookState.reserved:
+        return 'محجوز';
+      case enBookState.unavailable:
+        return 'غير متاح';
+      case enBookState.overdue:
+        return 'متأخر';
+    }
+  }
+
+  Color get backgroundColor {
+    switch (this) {
+      case enBookState.borrowable:
+        return Color(0xFFDCFCE7);
+      case enBookState.borrowed:
+        return Color(0xFFDBEAFE);
+      case enBookState.reservable:
+        return Color(0xFFFFEDD5);
+      case enBookState.reserved:
+        return Color(0xFFFEF9C3);
+      case enBookState.unavailable:
+        return Color(0xFFF3F4F6);
+      case enBookState.overdue:
+        return Color(0xFFFEE2E2);
+    }
+  }
+
+  IconData get icon {
+    switch (this) {
+      case enBookState.borrowable:
+        return Icons.check_circle;
+      case enBookState.borrowed:
+        return Icons.book;
+      case enBookState.reservable:
+        return Icons.bookmark_border;
+      case enBookState.reserved:
+        return Icons.access_time;
+      case enBookState.unavailable:
+        return Icons.warning;
+      case enBookState.overdue:
+        return Icons.error_outline;
+    }
+  }
+
+  Color get borderColor {
+    switch (this) {
+      case enBookState.borrowable:
+        return Color(0xFFBBF7D0);
+      case enBookState.borrowed:
+        return Color(0xFFBFDBFE);
+      case enBookState.reservable:
+        return Color(0xFFFED7AA);
+      case enBookState.reserved:
+        return Color(0xFFFEF08A);
+      case enBookState.unavailable:
+        return Color(0xFFE5E7EB);
+      case enBookState.overdue:
+        return Color(0xFFFECACA);
+    }
+  }
+
+  Color get textColor {
+    switch (this) {
+      case enBookState.borrowable:
+        return Color(0xFF166534);
+      case enBookState.borrowed:
+        return Color(0xFF1E40AF);
+      case enBookState.reservable:
+        return Color(0xFF9A3412);
+      case enBookState.reserved:
+        return Color(0xFF854D0E);
+      case enBookState.unavailable:
+        return Color(0xFF1F2937);
+      case enBookState.overdue:
+        return Color(0xFF991B1B);
+    }
+  }
 }
