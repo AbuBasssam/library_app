af5380c9954fbf1ecf4fe28fefb00e269650ea89
Abdul Rahman <programmerabubassam@gmail.com>
2025-10-08 15:27:28 +0300

    add test book details data

---
+++ lib/core/helpers/app_strings.dart
@@ -0,0 +1,35 @@
+class AppStrings {
+  AppStrings._();
+  // icons
+  static const String icRomance = 'assets/svgs/Romance.svg';
+  static const String icFantasy = 'assets/svgs/Fantasy.svg';
+  static const String icMystery = 'assets/svgs/Mystery.svg';
+  static const String icProgramming = 'assets/svgs/Programming.svg';
+  static const String icEconomic = 'assets/svgs/Economic.svg';
+  static const String icHistory = 'assets/svgs/History.svg';
+  static const String icScience = 'assets/svgs/Science.svg';
+  static const String icBook = 'assets/svgs/Book.svg';
+  // pathes
+  static const String translationfile = 'assets/translations';
+
+  // images
+  static const String imgWelcom = 'assets/images/WelcomeToBookWord.jpg';
+  static const String imgLogo = 'assets/svgs/BacaLogo.svg';
+  // Categories
+  // EN
+  static const String enRomance = 'Romance';
+  static const String enFantasy = 'Fantasy';
+  static const String enMystery = 'Mystery';
+  static const String enProgramming = 'Programming';
+  static const String enEconomic = 'Economics';
+  static const String enHistory = 'History';
+  static const String enScience = 'Science';
+  //AR
+  static const String arRomance = 'رومانسية';
+  static const String arFantasy = 'فانتازيا';
+  static const String arMystery = 'غموض';
+  static const String arProgramming = 'برمجة';
+  static const String arEconomic = 'إقتصاد';
+  static const String arHistory = 'تاريخ';
+  static const String arScience = 'علوم';
+}
