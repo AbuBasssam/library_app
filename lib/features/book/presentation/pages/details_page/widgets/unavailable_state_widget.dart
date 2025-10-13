20219323adc9cd4014628ec0d630d98c7bd0dd6e
Abdul Rahman <programmerabubassam@gmail.com>
2025-10-13 07:39:43 +0300

    make UnavailableStateWidget localizable

--- lib/features/book/presentation/pages/details_page/widgets/unavailable_state_widget.dart
+++ lib/features/book/presentation/pages/details_page/widgets/unavailable_state_widget.dart
@@ -1,12 +1,15 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:iconsax_plus/iconsax_plus.dart';
 import 'package:library_app/core/helpers/spacing.dart';
 import 'package:library_app/core/theme/app_colors.dart';
 import 'package:library_app/core/theme/app_styles.dart';
+import 'package:library_app/core/theme/font_weight_helper.dart';
 import 'package:library_app/features/book/presentation/pages/details_page/widgets/book_state_card.dart';
 import 'package:library_app/features/book/presentation/pages/details_page/widgets/primary_action_button.dart';
 import 'package:library_app/features/book/presentation/pages/details_page/widgets/wishlist_button.dart';
+import 'package:library_app/generated/locale_keys.g.dart';
 
 class UnavailableStateWidget extends StatelessWidget {
   final VoidCallback onNotifyPressed;
@@ -27,7 +30,7 @@ class UnavailableStateWidget extends StatelessWidget {
           borderColor: Colors.grey[300]!,
           icon: Icons.warning_amber_rounded,
           iconColor: Colors.grey[600]!,
-          title: 'Currently unavailable',
+          title: LocaleKeys.currently_unavailable.tr(),
           titleColor: Colors.grey[800]!,
           content: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -35,20 +38,16 @@ class UnavailableStateWidget extends StatelessWidget {
               RichText(
                 text: TextSpan(
                   style: AppStyles.font14RichBlackRegular.copyWith(
-                    color: Colors.grey[700],
+                    color: AppColors.gray700,
                     height: 1.5.h,
                   ),
-                  children: const [
+                  children: [
+                    TextSpan(text: LocaleKeys.unavailable_description.tr()),
                     TextSpan(
-                      text:
-                          'This book is not available for borrowing or reservation at the moment.\n',
-                    ),
-                    TextSpan(
-                      text:
-                          'Be the first to know when it becomes available again!',
+                      text: '\n${LocaleKeys.notify_availability.tr()}',
                       style: TextStyle(
                         color: AppColors.blue600,
-                        fontWeight: FontWeight.w600,
+                        fontWeight: FontWeightHelper.semiBold,
                       ),
                     ),
                   ],
@@ -60,7 +59,7 @@ class UnavailableStateWidget extends StatelessWidget {
                 child: PrimaryActionButton(
                   onPressed: onNotifyPressed,
                   icon: IconsaxPlusLinear.notification,
-                  label: 'Notify When Available',
+                  label: LocaleKeys.notify_me_button.tr(),
                   backgroundColor: Colors.grey[800]!,
                   foregroundColor: Colors.white,
                   padding: EdgeInsets.symmetric(vertical: 10.h),
