006ee842c18dbd08dc91f77d8c2136106652aa8b
Abdul Rahman <programmerabubassam@gmail.com>
2025-10-13 06:50:17 +0300

    mate rated widget localizable

--- lib/features/book/presentation/pages/details_page/widgets/rated_widget.dart
+++ lib/features/book/presentation/pages/details_page/widgets/rated_widget.dart
@@ -1,7 +1,11 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:library_app/core/helpers/spacing.dart';
+import 'package:library_app/core/theme/app_colors.dart';
+import 'package:library_app/core/theme/app_styles.dart';
+import 'package:library_app/generated/locale_keys.g.dart';
 
 class RatedWidget extends StatelessWidget {
   final int rating;
@@ -17,7 +21,7 @@ class RatedWidget extends StatelessWidget {
             Icon(Icons.star, size: 16.w, color: Colors.grey[700]),
             horizontalSpace(8),
             Text(
-              'Rate this book',
+              LocaleKeys.rate_book_title.tr(),
               style: TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.w600,
@@ -35,11 +39,10 @@ class RatedWidget extends StatelessWidget {
               color: Color(0xFF16A34A),
             ),
             horizontalSpace(8),
-            const Text(
-              'Thank you! Your rating has been recorded',
-              style: TextStyle(
-                fontSize: 14,
-                color: Color(0xFF16A34A),
+            Text(
+              LocaleKeys.your_rating.tr(),
+              style: AppStyles.font14RichBlackRegular.copyWith(
+                color: AppColors.green600,
               ),
             ),
           ],
@@ -51,7 +54,7 @@ class RatedWidget extends StatelessWidget {
           itemPadding: EdgeInsets.symmetric(horizontal: 2.0.w),
           itemBuilder: (context, _) => Icon(
             Icons.star,
-            color: const Color(0xFFEAB308),
+            color: AppColors.amberGlow,
           ),
         )
       ],
