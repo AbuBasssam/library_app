2113859b288f50dfa10f316bb1198242b2bb9083
Abdul Rahman <programmerabubassam@gmail.com>
2025-10-20 14:00:58 +0300

    Add BookListRepository to BookReppsstory

--- lib/features/book/data/repo/book_repository.dart
+++ lib/features/book/data/repo/book_repository.dart
@@ -1,14 +1,19 @@
 import 'package:library_app/core/helpers/api/api_helper.dart';
 import 'package:library_app/core/helpers/api/api_response.dart';
 import 'package:library_app/core/helpers/api/api_result.dart';
+import 'package:library_app/core/helpers/local_operation_result.dart';
+import 'package:library_app/features/book/data/models/book_data_hive_model.dart';
 import 'package:library_app/features/book/data/models/book_details.dart';
+import 'package:library_app/features/book/data/models/book_list_hive_model.dart';
+import 'package:library_app/features/book/data/repo/book_list_repository.dart';
 import 'package:library_app/features/book/data/services/book_service.dart';
 import 'package:library_app/features/book/domain/abstracts/book_repository.dart';
 
 class BookRepository implements IBookRepository {
   final BookService _service;
+  final BookListRepository _repo;
 
-  BookRepository(this._service);
+  BookRepository(this._service, this._repo);
   @override
   Future<ApiResult> getBookDetails(
     int bookId,
@@ -16,5 +21,54 @@ class BookRepository implements IBookRepository {
     return await ApiHelper.executeApiCall<ApiResponse<BookDetailsModel>>(
       () => _service.getBookDetails(bookId),
     );
+  }
+
+  @override
+  Future<LocalOperationResult<List<BookListHiveModel>>> getBookLists() async {
+    return await _repo.getBookLists();
+  }
+
+  @override
+  Future<LocalOperationResult<bool>> createList({
+    required String listName,
+  }) async {
+    return await _repo.createList(listName: listName);
+  }
+
+  @override
+  Future<LocalOperationResult<bool>> addBookToNewList({
+    required String listName,
+    required BookDataHiveModel bookData,
+  }) async {
+    return await _repo.addBookToNewList(
+      listName: listName,
+      bookData: bookData,
+    );
+  }
+
+  @override
+  Future<LocalOperationResult<bool>> addBookToList({
+    required int listId,
+    required BookDataHiveModel bookData,
+  }) {
+    return _repo.addBookToList(listId: listId, bookData: bookData);
+  }
+
+  @override
+  Future<LocalOperationResult<bool>> removeBookFromList({
+    required int listId,
+    required int bookId,
+  }) {
+    return _repo.removeBookFromList(listId: listId, bookId: bookId);
+  }
+
+  @override
+  Future<LocalOperationResult<bool>> deleteList(int listId) {
+    return _repo.deleteList(listId);
+  }
+
+  @override
+  Future<LocalOperationResult<bool>> isSavedBook(int bookId) {
+    return _repo.isSavedBook(bookId);
   }
 }
--- lib/features/book/domain/abstracts/book_repository.dart
+++ lib/features/book/domain/abstracts/book_repository.dart
@@ -1,5 +1,32 @@
 import 'package:library_app/core/helpers/api/api_result.dart';
+import 'package:library_app/core/helpers/local_operation_result.dart';
+import 'package:library_app/features/book/data/models/book_data_hive_model.dart';
+import 'package:library_app/features/book/data/models/book_list_hive_model.dart';
 
 abstract class IBookRepository {
   Future<ApiResult> getBookDetails(int bookId);
+  Future<LocalOperationResult<List<BookListHiveModel>>> getBookLists();
+
+  Future<LocalOperationResult<bool>> createList({
+    required String listName,
+  });
+
+  Future<LocalOperationResult<bool>> addBookToNewList({
+    required String listName,
+    required BookDataHiveModel bookData,
+  });
+
+  Future<LocalOperationResult<bool>> addBookToList({
+    required int listId,
+    required BookDataHiveModel bookData,
+  });
+
+  Future<LocalOperationResult<bool>> removeBookFromList({
+    required int listId,
+    required int bookId,
+  });
+
+  Future<LocalOperationResult<bool>> deleteList(int listId);
+
+  Future<LocalOperationResult<bool>> isSavedBook(int bookId);
 }
