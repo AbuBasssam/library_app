db7258970f479a4645256833a4ad980ba52f975a
Abdul Rahman <programmerabubassam@gmail.com>
2025-10-21 11:38:47 +0300

    Add Description to Spacing  methods

--- lib/core/helpers/spacing.dart
+++ lib/core/helpers/spacing.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+/// return a SizedBox with responsive hight
 SizedBox verticalSpace(double height) => SizedBox(height: height.h);
 
+/// return a SizedBox with responsive width
 SizedBox horizontalSpace(double width) => SizedBox(width: width.w);
