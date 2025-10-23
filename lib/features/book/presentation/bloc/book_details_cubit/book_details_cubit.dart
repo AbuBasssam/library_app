275a394cf5c081ebf0c1fa2beb64ee931dfa0d32
Abdul Rahman <programmerabubassam@gmail.com>
2025-10-23 11:55:23 +0300

    Modify BookDetailsCubit -Add saveBookToListData function refactor emitBookDetailsStates to use local implementation

--- lib/features/book/presentation/bloc/book_details_cubit/book_details_cubit.dart
+++ lib/features/book/presentation/bloc/book_details_cubit/book_details_cubit.dart
@@ -1,8 +1,20 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:library_app/core/helpers/api/api_response.dart';
+import 'package:library_app/core/helpers/app_strings.dart';
+import 'package:library_app/core/helpers/local_operation_result.dart';
 import 'package:library_app/features/book/data/models/book_details.dart';
+import 'package:library_app/features/book/data/models/book_meta.dart';
 import 'package:library_app/features/book/domain/abstracts/book_repository.dart';
+import 'package:library_app/features/book/domain/entities/book_entity.dart';
+import 'package:library_app/features/book/domain/entities/book_recommendations.dart';
+import 'package:library_app/features/book/domain/entities/book_status.dart';
+import 'package:library_app/features/book/domain/entities/en_book_state.dart';
+import 'package:library_app/features/book/domain/entities/user_book_preference.dart';
 import 'package:library_app/features/book/presentation/bloc/book_details_cubit/book_details_state.dart';
+import 'package:library_app/features/book/presentation/mappers/book_details_mapper.dart';
+import 'package:library_app/features/book/presentation/models/book_list_data.dart';
 
 class BookDetailsCubit extends Cubit<BookDetailsState> {
   final IBookRepository _repo;
@@ -11,17 +23,169 @@ class BookDetailsCubit extends Cubit<BookDetailsState> {
 
   Future<void> emitBookDetailsStates(int bookId) async {
     emit(const BookDetailsState.loading());
+    await _localeVersion(bookId);
+  }
 
+  Future<void> _localeVersion(int bookId) async {
+    final jsonString = await rootBundle.loadString(
+      AppStrings.borrowedBookDetails,
+    );
+    final jsonMap = jsonDecode(jsonString);
+    final responseMeta = jsonMap['meta'];
+    final responseData = jsonMap['data'];
+    final bookModel = BookDetailsModel.fromJson(responseData);
+    final bookMetaModel = BookMeta.fromJson(responseMeta);
+    final bookDetails = bookModel.toEntity();
+    final bookStauts = _mapToBookStauts(bookMetaModel, bookModel.bookState);
+
+    LocalOperationResult isSavedResult = await _repo.isSavedBook(bookId);
+
+    final userPreference = UserBookPreference(
+      bookId: bookDetails.id!,
+      userRating: bookModel.userRating,
+      isSaved: isSavedResult.isSuccess,
+    );
+    List<BookEntity> similarBooks = bookModel.similarBooks
+        .map(
+          (e) => BookEntity(
+            id: e.bookId,
+            imageUrl: e.coverImageUrl,
+            isNew: e.isNewBook,
+            title: e.title,
+            author: e.author,
+            rating: e.averageRating,
+          ),
+        )
+        .toList();
+    List<BookEntity> relatedBooks = bookModel.relatedBooks
+        // .where((rb) => rb.bookId != bookDetails.id)
+        .map(
+          (e) => BookEntity(
+            id: e.bookId,
+            imageUrl: e.coverImageUrl,
+            isNew: e.isNewBook,
+            title: e.title,
+            publishYear: e.publicationYear,
+            rating: e.averageRating,
+          ),
+        )
+        .toList();
+    final BookRecommendations recommendations = BookRecommendations(
+      similarBooks: similarBooks,
+      relatedBooks: relatedBooks,
+    );
+
+    emit(
+      BookDetailsState.success(
+          bookDetails: bookDetails,
+          bookStatus: bookStauts,
+          userPreference: userPreference,
+          recommendations: recommendations),
+    );
+  }
+
+  Future<void> _remoteVersion(int bookId) async {
     final response = await _repo.getBookDetails(bookId);
 
-    response.when(success: (response) {
-      final apiResponse = response as ApiResponse<BookDetailsModel>;
+    response.when(
+      success: (response) async {
+        final apiResponse = response as ApiResponse<BookDetailsModel>;
+        final bookModel = response.data;
+        final bookDetails = bookModel.toEntity();
+        final meta = (apiResponse.meta as Map<String, dynamic>);
+        final metaData = BookMeta(
+          maxBorrowingDuration: meta['maxBorrowingDuration'],
+          recommededBorrowingDuration: meta['recommededBorrowingDuration'],
+          finePerDay: meta['finePerDay'],
+          dueDate: meta['dueDate'],
+          estimatedFine: meta['estimatedFine'],
+          estimatedAvailableDate: meta['estimatedAvailableDate'],
+          peopleAhead: meta['peopleAhead'],
+          pickupTimeRemainder: meta['maxBorrowingDuration'],
+        );
 
-      emit(BookDetailsState.success(data: apiResponse.data));
-    }, failure: (error) {
-      String errMessage = error.apiErrorModel.message;
+        final bookStauts =
+            _mapToBookStauts(metaData, apiResponse.data.bookState);
 
-      emit(BookDetailsState.failure(message: errMessage));
-    });
+        LocalOperationResult isSavedResult = await _repo.isSavedBook(bookId);
+        bool isSaved = isSavedResult.isSuccess;
+
+        final userPreference = UserBookPreference(
+          bookId: bookModel.bookId,
+          userRating: bookModel.userRating,
+          isSaved: isSaved,
+        );
+        List<BookEntity> similarBooks = bookModel.similarBooks
+            .map(
+              (e) => BookEntity(
+                id: e.bookId,
+                imageUrl: e.coverImageUrl,
+                isNew: e.isNewBook,
+                title: e.title,
+                author: e.author,
+                rating: e.averageRating,
+              ),
+            )
+            .toList();
+        List<BookEntity> relatedBooks = bookModel.relatedBooks
+            // .where((rb) => rb.bookId != bookDetails.id)
+            .map(
+              (e) => BookEntity(
+                id: e.bookId,
+                imageUrl: e.coverImageUrl,
+                isNew: e.isNewBook,
+                title: e.title,
+                publishYear: e.publicationYear,
+                rating: e.averageRating,
+              ),
+            )
+            .toList();
+        final BookRecommendations recommendations = BookRecommendations(
+          similarBooks: similarBooks,
+          relatedBooks: relatedBooks,
+        );
+
+        emit(
+          BookDetailsState.success(
+            bookDetails: bookDetails,
+            bookStatus: bookStauts,
+            userPreference: userPreference,
+            recommendations: recommendations,
+          ),
+        );
+      },
+      failure: (error) {
+        String errMessage = error.apiErrorModel.message;
+
+        emit(BookDetailsState.failure(message: errMessage));
+      },
+    );
+  }
+
+  BookListData? saveBookToListData() {
+    if (state is! Success) return null;
+
+    final data = (state as Success).bookDetails;
+    return BookListData(
+      bookId: data.id!,
+      coverImage: data.imageUrl!,
+      title: data.title!,
+      author: data.author!,
+    );
+  }
+
+  BookStatus _mapToBookStauts(BookMeta bookMeta, enBookState bookStatue) {
+    return BookStatus(
+      state: bookStatue,
+      maxBorrowingDuration: bookMeta.maxBorrowingDuration,
+      recommededBorrowingDuration: bookMeta.recommededBorrowingDuration,
+      pickupRequiredHours: bookMeta.pickupRequiredHours,
+      finePerDay: bookMeta.finePerDay,
+      dueDate: bookMeta.dueDate,
+      estimatedFine: bookMeta.estimatedFine,
+      estimatedAvailableDate: bookMeta.estimatedAvailableDate,
+      peopleAhead: bookMeta.peopleAhead,
+      pickupTimeRemainder: bookMeta.pickupTimeRemainder,
+    );
   }
 }
